323332361c8b36ad47bb16fd714ef722cb11e147
protapkrghosh <protapg518@gmail.com>
2025-03-05 21:23:18 +0600

    pin validation

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-   console.log('clicked login button');
+document.getElementById("login-btn").addEventListener("click", function (event) {
+   event.preventDefault();
+   const accountNumber = document.getElementById("accountNumber").value;
+   const pinNumber = document.getElementById("pinNumber").value;
+   const pin = parseInt(pinNumber);
+   
+   if (accountNumber.length === 11) {
+      if (pin === 107754) {
+         console.log("Your pin is correct!");
+      }
+      else {
+         console.log("Incorrect pin!");
+      }
+   }
+   else {
+      alert('Please input account number 11 digits');
+   }
 });
 
 
